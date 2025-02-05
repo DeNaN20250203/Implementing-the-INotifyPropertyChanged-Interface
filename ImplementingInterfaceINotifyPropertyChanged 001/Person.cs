12c424258062f43211fa56acf1fdbf1f2b39f513
ДеЮра <doomov@yandex.ru>
2025-02-05 14:12:08 +0700

    Add files via upload

---
+++ ImplementingInterfaceINotifyPropertyChanged 001/Person.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace ImplementingInterfaceINotifyPropertyChanged_001
+{
+	/// <summary>
+	/// Определяем класс Person, который реализует интерфейс INotifyPropertyChanged
+	/// </summary>
+	public class Person : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Закрытое поле для хранения имени
+		/// </summary>
+		private string? _name;
+
+		/// <summary>
+		/// Свойство Name с геттером и сеттером
+		/// </summary>
+		public string? Name
+		{
+			get => _name; // Возвращаем текущее значение имени
+			set
+			{
+				_name = value; // Устанавливаем новое значение имени
+							   // Вызываем событие PropertyChanged, чтобы уведомить об изменении свойства
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+			}
+		}
+
+		/// <summary>
+		/// Событие, которое срабатывает при изменении свойства
+		/// </summary>
+		public event PropertyChangedEventHandler? PropertyChanged;
+	}
+}
---
+++ ImplementingInterfaceINotifyPropertyChanged 001/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ImplementingInterfaceINotifyPropertyChanged_001
+{
+	internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+			// Создаем объект класса Person и устанавливаем его имя "Sarah"
+			var myClass = new Person { Name = "Sarah" };
+			// Выводим текущее имя в консоль
+			Console.WriteLine(myClass.Name);
+
+			// Ждем 200 миллисекунд
+			await Task.Delay(200);
+			// Изменяем имя объекта на "NewName"
+			myClass.Name = "NewName";
+			// Выводим новое имя в консоль
+			Console.WriteLine(myClass.Name);
+			Console.WriteLine();
+			Console.ReadKey();
+        }
+    }
+}
